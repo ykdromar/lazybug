582b20aa210255b6f6fc460788943687c316b934
Yash <ykdromarpro@gmail.com>
2022-12-13 13:05:47 +0530

    project structure setuped

---
+++ src/api/index.js
@@ -0,0 +1,44 @@
+import { API_URLS, LOCALSTORAGE_TOKEN_KEY } from "../utils";
+const customFetch = async (url, { body, ...customConfig }) => {
+  const token = window.localStorage.getItem(LOCALSTORAGE_TOKEN_KEY);
+  const headers = {
+    "content-type": "application/json",
+    Accept: "application/json",
+  };
+  if (token) {
+    headers.Authorization = `Bearer ${token}`;
+  }
+  const config = {
+    ...customConfig,
+    headers: {
+      ...headers,
+      ...customConfig.headers,
+    },
+  };
+  if (body) {
+    config.body = JSON.stringify(body);
+  }
+  try {
+    const response = await fetch(url, config);
+    const data = await response.json();
+    if (data.success) {
+      return {
+        data: data.data,
+        success: true,
+      };
+    }
+    // console.log(response);
+    throw new Error(data.message);
+  } catch (error) {
+    console.log("Error in Fetching Data: ", error);
+    return {
+      message: error.message,
+      success: false,
+    };
+  }
+};
+
+const getPosts = (page = 1, limit = 10) => {
+  return customFetch(API_URLS.posts(page, limit), { method: "GET" });
+};
+export { getPosts };
---
+++ src/utils/index.js
@@ -0,0 +1,3 @@
+// import { API_URLS, LOCALSTORAGE_TOKEN_KEY } from "./constants";
+// export { API_URLS, LOCALSTORAGE_TOKEN_KEY };
+export * from "./constants";
